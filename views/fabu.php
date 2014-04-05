11640a2bde9dac3d4811383c1072ee49dafe1f38
Mld <463471808@qq.com>
2014-04-05 21:21:15 +0800

    test

--- views/fabu.php
+++ views/fabu.php
@@ -6,7 +6,7 @@
 
 <body>
 <div id="box">
-	<div class="title">发布新闻</div>
+	<div class="title">发布新闻 </div>
 		<div class="right3">欢迎您！<?php echo $username ?>&nbsp;&nbsp;
 		<a href="/CI/index.php/home/loginout">注销</a>
 	</div>
